16fdeac03f6e4155e5e420bc7c5ff22339ffe25a
tsubasaozawa <tsubasaozawa@users.noreply.github.com>
2020-01-27 17:47:09 +0900

    微修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,4 +24,15 @@ namespace :deploy do
   end
 end
 
+desc 'db_seed'
+task :db_seed do
+  on roles(:db) do |host|
+    with rails_env: fetch(:rails_env) do
+      within current_path do
+        execute :bundle, :exec, :rake, 'db:seed'
+      end
+    end
+  end
+end
+
 set :linked_files, fetch(:linked_files, []).push("config/master.key")
